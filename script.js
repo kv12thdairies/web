55ed5588ebe1fc1f5688c85dad6ca674a25ff6d6
kv12thdairies <deepak210808@gmail.com>
2026-03-05 13:00:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -311,7 +311,7 @@ const commentInput = document.getElementById("commentInput");
 const commentName = document.getElementById("commentName");
 const addCommentBtn = document.getElementById("addCommentBtn");
 const commentsBoard = document.getElementById("commentsBoard");
-const gratitudeList = ["Utkarsh"];
+const gratitudeList = ["Subhraj singh","Dhruv"];
 function renderCommentNote(name, text, time) {
 
   const colors = ["yellow", "blue", "pink", "green"];
